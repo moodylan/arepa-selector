0f06b1ea03aff2672a5db613b2e2610cb8a1d3d4
Dylan Moo Hernandez <dylangmooh@gmail.com>
2025-03-07 14:35:36 -0500

    Implement state management, add event handlers, and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => {
+    setCount(count + multiplier);
+  };
+
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+      setCount(count - 10);
+    }
+  };
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  };
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier(multiplier * 10);
+      setCount(count - 1000);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>Arepa Selector</h1>
+      <h2>Count: {count}</h2>
+      <img
+        className="arepa"
+        src="grilled-arepa.webp"
+        onClick={updateCount}
+      ></img>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 arepas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 arepas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 arepas</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
